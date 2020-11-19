a64c9e4b94bfbcd4c76e5e8d96b6a4772fc19ad7
CAMERON MACKENZIE <S6027509@live.tees.ac.uk>
2020-11-19 14:32:17 +0000

    Added SysLogsDbContext

---
+++ SysLogs/Data/SysLogsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SystemLogs.Models;
+
+namespace SysLogs.Data
+{
+    public class SysLogsDbContext : DbContext
+    {
+        public DbSet<SystemLog> SystemLogs { get; set; }
+
+        public SysLogsDbContext(DbContextOptions<SysLogsDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
