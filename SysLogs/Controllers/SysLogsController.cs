3e85f5abc72a3fce5ac5e305175a292a589448f3
CAMERON MACKENZIE <S6027509@live.tees.ac.uk>
2020-11-19 13:37:52 +0000

    Add project files.

---
+++ SysLogs/Controllers/SysLogsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SystemLogs.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SysLogsController : ControllerBase
+    {
+
+        private readonly ILogger<SysLogsController> _logger;
+
+        public SysLogsController(ILogger<SysLogsController> logger)
+        {
+            _logger = logger;
+        }
+
+        //Store any system logs sent here in internal database
+        [HttpPost]
+        public async Task<IActionResult> StoreSystemLog([FromRoute] int componentId, DateTime date, string alertType)
+        {
+
+        }
+
+        //Send unfiltered list of logs (Maybe implement pagination or some kind of arbitrary max list size)
+        [HttpGet]
+        public async Task<IActionResult> GetAllSystemLogs()
+        {
+
+        }
+
+        //Send list of event logs specifically for the given user
+        [HttpGet]
+        public async Task<IActionResult> GetUserSystemLogs([FromRoute] string user)
+        {
+
+        }
+
+        //Send Filtered list of logs
+        [HttpGet]
+        public async Task<IActionResult> GetFilteredSystemLogs([FromBody] int componentId, DateTime date, string alertType)
+        {
+
+        }
+    }
+}
---
+++ SysLogs/Models/SystemLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SystemLogs.Models
+{
+    public class SystemLog
+    {
+        public int LogId { get; set; }
+        public int ComponentId { get; set; }
+        public string Details { get; set; }
+        public DateTime Date { get; set; }
+        public string AlertType { get; set; }
+    }
+}
